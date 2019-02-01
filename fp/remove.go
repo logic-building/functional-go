f7fc4ea83351873f966ffda0253e8b88789840db
Sah, Nandeshwar <NANDESHWAR.SAH@dish.com>
2019-02-01 14:10:20 -0700

    Added docs for function remove

--- fp/remove.go
+++ fp/remove.go
@@ -1,5 +1,21 @@
 package fp
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveInt(isEven, []int{1, 2, 3, 4}) // Returns: [1, 3]
+//
+//	func isEven(num int) bool {
+//		return num%2 == 0
+//	}
 func RemoveInt(f func(int) bool, list []int) []int {
 	if f == nil {
 		return []int{}
@@ -13,6 +29,22 @@ func RemoveInt(f func(int) bool, list []int) []int {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveInt64(isEven, []int64{1, 2, 3, 4}) // Returns: [1, 3]
+//
+//	func isEven(num int64) bool {
+//		return num%2 == 0
+//	}
 func RemoveInt64(f func(int64) bool, list []int64) []int64 {
 	if f == nil {
 		return []int64{}
@@ -26,6 +58,22 @@ func RemoveInt64(f func(int64) bool, list []int64) []int64 {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveInt32(isEven, []int32{1, 2, 3, 4}) // Returns: [1, 3]
+//
+//	func isEven(num int32) bool {
+//		return num%2 == 0
+//	}
 func RemoveInt32(f func(int32) bool, list []int32) []int32 {
 	if f == nil {
 		return []int32{}
@@ -39,6 +87,22 @@ func RemoveInt32(f func(int32) bool, list []int32) []int32 {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveInt16(isEven, []int16{1, 2, 3, 4}) // Returns: [1, 3]
+//
+//	func isEven(num int16) bool {
+//		return num%2 == 0
+//	}
 func RemoveInt16(f func(int16) bool, list []int16) []int16 {
 	if f == nil {
 		return []int16{}
@@ -52,6 +116,22 @@ func RemoveInt16(f func(int16) bool, list []int16) []int16 {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveInt8(isEven, []int8{1, 2, 3, 4}) // Returns: [1, 3]
+//
+//	func isEven(num int8) bool {
+//		return num%2 == 0
+//	}
 func RemoveInt8(f func(int8) bool, list []int8) []int8 {
 	if f == nil {
 		return []int8{}
@@ -65,6 +145,22 @@ func RemoveInt8(f func(int8) bool, list []int8) []int8 {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveUint64(isEven, []uint64{1, 2, 3, 4}) // Returns: [1, 3]
+//
+//	func isEven(num uint64) bool {
+//		return num%2 == 0
+//	}
 func RemoveUint64(f func(uint64) bool, list []uint64) []uint64 {
 	if f == nil {
 		return []uint64{}
@@ -78,6 +174,22 @@ func RemoveUint64(f func(uint64) bool, list []uint64) []uint64 {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveUint32(isEven, []uint32{1, 2, 3, 4}) // Returns: [1, 3]
+//
+//	func isEven(num uint32) bool {
+//		return num%2 == 0
+//	}
 func RemoveUint32(f func(uint32) bool, list []uint32) []uint32 {
 	if f == nil {
 		return []uint32{}
@@ -91,6 +203,22 @@ func RemoveUint32(f func(uint32) bool, list []uint32) []uint32 {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveUint16(isEven, []uint16{1, 2, 3, 4}) // Returns: [1, 3]
+//
+//	func isEven(num uint16) bool {
+//		return num%2 == 0
+//	}
 func RemoveUint16(f func(uint16) bool, list []uint16) []uint16 {
 	if f == nil {
 		return []uint16{}
@@ -104,6 +232,22 @@ func RemoveUint16(f func(uint16) bool, list []uint16) []uint16 {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveUint8(isEven, []uint8{1, 2, 3, 4}) // Returns: [1, 3]
+//
+//	func isEven(num uint8) bool {
+//		return num%2 == 0
+//	}
 func RemoveUint8(f func(uint8) bool, list []uint8) []uint8 {
 	if f == nil {
 		return []uint8{}
@@ -117,6 +261,22 @@ func RemoveUint8(f func(uint8) bool, list []uint8) []uint8 {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveUint(isEven, []uint{1, 2, 3, 4}) // Returns: [1, 3]
+//
+//	func isEven(num uint) bool {
+//		return num%2 == 0
+//	}
 func RemoveUint(f func(uint) bool, list []uint) []uint {
 	if f == nil {
 		return []uint{}
@@ -130,6 +290,22 @@ func RemoveUint(f func(uint) bool, list []uint) []uint {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveFloat64(isPositive, []float64{0, -2, 2, 40.50}) // Returns: [0, -2]
+//
+//	func isPositive(num float64) bool {
+//		return num > 0
+//	}
 func RemoveFloat64(f func(float64) bool, list []float64) []float64 {
 	if f == nil {
 		return []float64{}
@@ -143,6 +319,22 @@ func RemoveFloat64(f func(float64) bool, list []float64) []float64 {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveFloat32(isPositive, []float32{0, -2, 2, 40.50}) // Returns: [0, -2]
+//
+//	func isPositive(num float32) bool {
+//		return num > 0
+//	}
 func RemoveFloat32(f func(float32) bool, list []float32) []float32 {
 	if f == nil {
 		return []float32{}
@@ -156,6 +348,22 @@ func RemoveFloat32(f func(float32) bool, list []float32) []float32 {
 	return newList
 }
 
+// Remove items from a list based on supplied function and returns new list
+//
+// Takes 2 inputs:
+//	1. Function
+//	2. List
+//
+// Returns:
+//	New List
+//	Empty list if both of arguments are nil or either one is nil.
+//
+// Example:
+//	fp.RemoveStr(isStringLengthLessThan10, []string{"gopal", "govinda", "Nandeshwar"}) // Returns: ["Nandeshwar"]
+//
+//	func isStringLengthLessThan10(str string) bool {
+//		return len(str) < 10
+//	}
 func RemoveStr(f func(string) bool, list []string) []string {
 	if f == nil {
 		return []string{}
