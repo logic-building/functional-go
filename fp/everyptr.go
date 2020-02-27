c716476c3c2d45fdcce99a764e8741f8639e9397
Sah, Nandeshwar <Nandeshwa.Sah@echostar.com>
2020-02-26 17:06:43 -0700

    Added everyptr function

---
+++ fp/everyptr.go
@@ -0,0 +1,197 @@
+package fp
+
+// EveryIntPtr returns true if supplied function returns logical true for every item in the list
+
+func EveryIntPtr(f func(*int) bool, list []*int) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryInt64Ptr returns true if supplied function returns logical true for every item in the list
+
+func EveryInt64Ptr(f func(*int64) bool, list []*int64) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryInt32Ptr returns true if supplied function returns logical true for every item in the list
+
+func EveryInt32Ptr(f func(*int32) bool, list []*int32) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryInt16Ptr returns true if supplied function returns logical true for every item in the list
+
+func EveryInt16Ptr(f func(*int16) bool, list []*int16) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryInt8Ptr returns true if supplied function returns logical true for every item in the list
+
+func EveryInt8Ptr(f func(*int8) bool, list []*int8) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryUintPtr returns true if supplied function returns logical true for every item in the list
+
+func EveryUintPtr(f func(*uint) bool, list []*uint) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryUint64Ptr returns true if supplied function returns logical true for every item in the list
+
+func EveryUint64Ptr(f func(*uint64) bool, list []*uint64) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryUint32Ptr returns true if supplied function returns logical true for every item in the list
+
+func EveryUint32Ptr(f func(*uint32) bool, list []*uint32) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryUint16Ptr returns true if supplied function returns logical true for every item in the list
+
+func EveryUint16Ptr(f func(*uint16) bool, list []*uint16) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryUint8Ptr returns true if supplied function returns logical true for every item in the list
+
+func EveryUint8Ptr(f func(*uint8) bool, list []*uint8) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryStrPtr returns true if supplied function returns logical true for every item in the list
+
+func EveryStrPtr(f func(*string) bool, list []*string) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryBoolPtr returns true if supplied function returns logical true for every item in the list
+
+func EveryBoolPtr(f func(*bool) bool, list []*bool) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryFloat32Ptr returns true if supplied function returns logical true for every item in the list
+
+func EveryFloat32Ptr(f func(*float32) bool, list []*float32) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// EveryFloat64Ptr returns true if supplied function returns logical true for every item in the list
+
+func EveryFloat64Ptr(f func(*float64) bool, list []*float64) bool {
+	if f == nil || len(list) == 0 {
+		return false
+	}
+	for _, v := range list {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
---
+++ internal/template/basic/everyptrtest.go
@@ -0,0 +1,66 @@
+package basic
+
+// Every<FTYPE>Ptr applies the function(1st argument) on each item of the list and returns new list
+func EveryPtrTest() string {
+	return `
+func TestEvery<FTYPE>Ptr(t *testing.T) {
+	// Test : every value in the list is even number
+	var v2 <TYPE> = 2
+	var v4 <TYPE> = 4
+	var v5 <TYPE> = 5
+	var v8 <TYPE> = 8
+	var v10 <TYPE> = 10
+	list1 := []*<TYPE>{&v8, &v2, &v10, &v4}
+	if !Every<FTYPE>Ptr(isEven<FTYPE>Ptr, list1) {
+		t.Errorf("Every<FTYPE>Ptr failed. Expected=true, actual=false")
+	}
+
+	list2 := []*<TYPE>{&v8, &v2, &v10, &v5, &v4}
+	if Every<FTYPE>Ptr(isEven<FTYPE>Ptr, list2) {
+		t.Errorf("Every<FTYPE>Ptr failed. Expected=false, actual=true")
+	}
+
+	if Every<FTYPE>Ptr(isEven<FTYPE>Ptr, nil) {
+		t.Errorf("Every<FTYPE>Ptr failed. Expected=false, actual=true")
+	}
+
+	if Every<FTYPE>Ptr(isEven<FTYPE>Ptr, []*<TYPE>{}) {
+		t.Errorf("Every<FTYPE>Ptr failed. Expected=false, actual=true")
+	}
+
+	if Every<FTYPE>Ptr(nil, []*<TYPE>{}) {
+		t.Errorf("Every<FTYPE>Ptr failed. Expected=false, actual=true")
+		t.Errorf(reflect.String.String())
+	}
+}
+`
+}
+
+// Every<FTYPE>Ptr applies the function(1st argument) on each item of the list and returns new list
+func EveryPtrBoolTest() string {
+	return `
+func TestEvery<FTYPE>Ptr(t *testing.T) {
+	var vt <TYPE> = true
+
+	// Test : every value in the list is either true or false
+	list1 := []*<TYPE>{&vt, &vt, &vt, &vt}
+	if !Every<FTYPE>Ptr(TruePtr, list1) {
+		t.Errorf("Every<FTYPE>Ptr failed. Expected=true, actual=false")
+	}
+
+	
+	list1 = []*bool{}
+	if Every<FTYPE>Ptr(TruePtr, list1) {
+		t.Errorf("EveryBool failed. Expected=false, actual=true")
+	}
+
+	if EveryBool(True, nil) {
+		t.Errorf("EveryBool failed. Expected=false, actual=true")
+	}
+}
+
+func TruePtr(val *bool) bool {
+	return *val == true
+}
+`
+}
