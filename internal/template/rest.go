8c6b31401aa0adc566af29671933a37baa77a093
Sah, Nandeshwar <Nandeshwa.Sah@echostar.com>
2020-04-01 12:24:33 -0600

    Auto-generated code has pointer version of function - Rest

--- internal/template/rest.go
+++ internal/template/rest.go
@@ -23,3 +23,29 @@ func Rest<CONDITIONAL_TYPE>(l []<TYPE>) []<TYPE> {
 }
 `
 }
+
+// RestPtr removes 1st item of the list and return new list having rest of the items
+func RestPtr() string {
+	return `
+
+// Rest<CONDITIONAL_TYPE> removes 1st item of the list and return new list having rest of the items
+func Rest<CONDITIONAL_TYPE>Ptr(l []*<TYPE>) []*<TYPE> {
+	if l == nil {
+		return []*<TYPE>{}
+	}
+
+	len := len(l)
+	if len == 0 || len == 1 {
+		return []*<TYPE>{}
+	}
+
+	newList := make([]*<TYPE>, len-1)
+
+	for i, v := range l[1:] {
+		newList[i] = v
+	}
+
+	return newList
+}
+`
+}
