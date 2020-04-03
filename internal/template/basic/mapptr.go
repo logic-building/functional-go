396489cddbdbee466de77082873adc98f174a602
Sah, Nandeshwar <Nandeshwa.Sah@echostar.com>
2020-04-03 17:10:57 -0600

    Added function MapErr which also returns error with new list

--- internal/template/basic/mapptr.go
+++ internal/template/basic/mapptr.go
@@ -26,7 +26,7 @@ func Map<FTYPE>Ptr(f func(*<TYPE>) *<TYPE>, list []*<TYPE>) []*<TYPE> {
 func MapPtrErr() string {
 	return `
 // Map<FTYPE>PtrErr takes 2 arguments:
-//  1. A function input argument: <TYPE> and return types (<TYPE>, error)
+//  1. A function input argument: *<TYPE> and return types (*<TYPE>, error)
 //  2. A list of type []*<TYPE>
 
 // Returns:
@@ -36,6 +36,32 @@ func Map<FTYPE>PtrErr(f func(*<TYPE>) (*<TYPE>, error), list []*<TYPE>) ([]*<TYP
 		return []*<TYPE>{}, nil
 	}
 	newList := make([]*<TYPE>, len(list))
+	for i, v := range list {
+		var err error
+		newList[i], err = f(v)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return newList, nil
+}
+`
+}
+
+// Map<FTYPE>Err applies the function(1st argument) on each item of the list and returns new list
+func MapErr() string {
+	return `
+// Map<FTYPE>Err takes 2 arguments:
+//  1. A function input argument: <TYPE> and return types (<TYPE>, error)
+//  2. A list of type []<TYPE>
+
+// Returns:
+// 	([]<TYPE>, error)
+func Map<FTYPE>Err(f func(<TYPE>) (<TYPE>, error), list []<TYPE>) ([]<TYPE>, error) {
+	if f == nil {
+		return []<TYPE>{}, nil
+	}
+	newList := make([]<TYPE>, len(list))
 	for i, v := range list {
 		var err error
 		newList[i], err = f(v)
